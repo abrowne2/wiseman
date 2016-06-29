758b4203d4649965eec4a56617a6596a896ed070
Adam Browne <adamo.browne@gmail.com>
2016-06-29 10:04:09 -0400

    first commit.

---
+++ app.js
@@ -0,0 +1,16 @@
+
+'use strict';
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var wise = require('./wise/wisdom.js');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/wise', function (req, res) {
+  res.json({text: wise, response_type: "in_channel"});
+});
+
+app.listen(process.env.PORT || 8080);
---
+++ mind/list.js
@@ -0,0 +1,10 @@
+module.exports = [
+  "A bird in the hand is worth two in the bush. Unless the bush is Austrailia, then get those birds out of your hand.\nThey're going to kill you.",
+  "A frog is faster than a toad. Unless, of course, the toad is sleeping.",
+  "If you take it with another grain of salt, your blood pressure will go up.",
+  "Time flies when you're having fun: Be boring.",
+  "A closed mouth gathers no feet.",
+  "All would live long but none would be old.",
+  "An apple a day keeps the doctor away, but an onion a day keeps everyone away.",
+  "It's better to s"
+]
