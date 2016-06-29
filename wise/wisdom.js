af3ef58e787f9fba5110c6eeaf3cc6327ea1af98
Adam Browne <adamo.browne@gmail.com>
2016-06-29 10:09:24 -0400

    Fix not computing random value.

--- wise/wisdom.js
+++ wise/wisdom.js
@@ -3,7 +3,8 @@ var knowledge = require('../mind/list')
 function fetchKnowledge() {
   //random index from js array ->
   // http://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
-  return "A wise man once said,\n" + knowledge[Math.floor(Math.random()*knowledge.length)];
+  var randIndex = Math.floor(Math.random()*knowledge.length);
+  return "A wise man once said,\n" + knowledge[randIndex];
 }
 
 module.exports = fetchKnowledge();
