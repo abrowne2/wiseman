4ca36d08bd982575d60b8f7c8cca4cbb470ead5f
Adam Browne <adamo.browne@gmail.com>
2016-06-29 12:20:50 -0400

    no cache

--- app.js
+++ app.js
@@ -8,10 +8,14 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/wise', function (req, res) {
+function nocache(req, res, next) {
   res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
   res.header('Expires', '-1');
   res.header('Pragma', 'no-cache');
+  next();
+}
+
+app.get('/wise', nocache, function (req, res) {
   res.json({text: wise, response_type: "in_channel"});
 });
 
