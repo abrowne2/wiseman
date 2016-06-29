b136910d6ae3f80455da20e20b7d613a8e4be02c
Adam Browne <adamo.browne@gmail.com>
2016-06-29 10:52:05 -0400

    stop stupid caching.

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/wise', function (req, res) {
+app.get('/wise?_=' + new Date().getTime(), function (req, res) {
   res.header('Cache-Control', 'no-store');
   res.json({text: wise, response_type: "in_channel"});
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(3000);
