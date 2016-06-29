eaf0e0a3de7804deca523192e0a6036a29d0b644
Adam Browne <adamo.browne@gmail.com>
2016-06-29 11:07:09 -0400

    Fix port.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+'use strict';
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -12,4 +12,4 @@ app.get('/wise', function (req, res) {
   res.json({text: wise, response_type: "in_channel"});
 });
 
-app.listen(process.ENV.port || 8080);
+app.listen(process.env.PORT || 8080);
