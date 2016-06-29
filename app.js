cdf4ea4ac015b23942b62b8b00454290606e62c7
Adam Browne <adamo.browne@gmail.com>
2016-06-29 11:29:26 -0400

    express.static

--- app.js
+++ app.js
@@ -3,13 +3,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var wise = require('./wise/wisdom.js');
-var app = express();
+var app = express.static();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/wise', function (req, res) {
-  res.setHeader('Cache-Control', 'public, max-age=31557600');
+  res.header('Cache-Control', 'no-cache');
   res.json({text: wise, response_type: "in_channel"});
 });
 
