9787e558f3a282e9d78a17dddf3206f93840f044
Adam Browne <adamo.browne@gmail.com>
2016-06-29 10:45:59 -0400

    randIndex

--- wise/wisdom.js
+++ wise/wisdom.js
@@ -5,7 +5,7 @@ function fetchKnowledge() {
   // http://stackoverflow.com/questions/5915096/get-random-item-from-javascript-array
   var randIndex = Math.floor(Math.random()*knowledge.length);
   var noCache = "A wise man once said,\n" + knowledge[randIndex];
-  return noCache;
+  return randIndex;
 }
 
 module.exports = fetchKnowledge();
